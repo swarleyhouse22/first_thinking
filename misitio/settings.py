2c2e8809538a2f3ef824ae9da96b08ad9a592973
franco <franco.a.riquelme@gmail.com>
2019-12-19 14:36:08 -0300

    Cuarto Commit, modificaciones de 'mi sitio'

--- misitio/settings.py
+++ misitio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django.urls import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*a4t44#^j(#cd$gviy)$myt@x8q5y4v_9fyryt402geyk38v0)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
